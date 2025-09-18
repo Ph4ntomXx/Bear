5bf166acee7a2b226150ee9c33af5b6bce9fc6d7
Ph4ntomXx <ph4ntomxry@gmail.com>
2025-09-18 17:00:28 +0500

    0.1.1 update

--- django_test/main/views.py
+++ django_test/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the main index.")
+
+def home(request):
+    return render(request, 'main/home.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
