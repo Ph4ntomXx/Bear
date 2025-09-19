f4fc2861d1c3b8aa66727eb9fe645933adf5f28d
Ph4ntomXx <ph4ntomxry@gmail.com>
2025-09-19 18:35:30 +0500

    v0.1.3: add news section (beta)

--- django_test/news/views.py
+++ django_test/news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import News
 
 def news_home(request):
-    return render(request, 'news/news_home.html')
+    news = News.objects.order_by('-published_date')
+    return render(request, 'news/news_home.html', {'news': news})
