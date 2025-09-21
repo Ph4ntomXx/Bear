87ba566667ef3b954dbf935e2fab3d07d07b9cca
Ph4ntomXx <ph4ntomxry@gmail.com>
2025-09-21 12:27:59 +0500

    git commit -m "v0.1.5: Django project setup with security settings and .env (beta)"

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# django-test-work
+# Bear
--- django_test/django_test/settings.py
+++ django_test/django_test/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,14 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
- 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n1e&2nko)*_qb6q5ctw#cnm&08r&p#z%7&f#7o6n@1j_%dh3_w'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
