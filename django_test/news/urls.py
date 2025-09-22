1f639e3bc72747f96c0e9d32d8deb24518c24c5c
Ph4ntomXx <ph4ntomxry@gmail.com>
2025-09-22 15:33:58 +0500

    v0.1.5: added DetailView (beta)

--- django_test/news/urls.py
+++ django_test/news/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create/', views.create_news, name='create_news'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news_detail'),
 ]
--- django_test/news/views.py
+++ django_test/news/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import News
 from .forms import NewsForm
+from django.views.generic import DetailView
+
 
 def news_home(request):
     news = News.objects.order_by('-published_date')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = News
+    template_name = 'news/news_detail.html'
+    context_object_name = 'news_item'
 
 def create_news(request):
     if request.method == 'POST':
